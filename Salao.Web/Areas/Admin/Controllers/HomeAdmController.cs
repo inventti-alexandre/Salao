c7db84db0c2badd78f5ce1e3932393f27e418dbe
jbalessandro <jb.alessandro@gmail.com>
2015-09-15 12:37:08 -0300

    Login completo

---
+++ Salao.Web/Areas/Admin/Controllers/HomeAdmController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salao.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class HomeAdmController : Controller
+    {
+        //
+        // GET: /Admin/HomeAdm/
+        public ActionResult Index(string message = "")
+        {
+            ViewBag.Message = message;
+            return View();
+        }
+	}
+}
