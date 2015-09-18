6047dee00058442bf8c4e81bd4506a76c98fcd56
jbalessandro <jb.alessandro@gmail.com>
2015-09-18 16:44:29 -0300

    cadastro empresa

---
+++ Salao.Domain/Abstract/Admin/ICadastroEmpresa.cs
@@ -0,0 +1,10 @@
+﻿using Salao.Domain.Models.Cliente;
+
+namespace Salao.Domain.Abstract.Admin
+{
+    public interface ICadastroEmpresa
+    {
+        int ChecarCadastroAnterior(string documento);
+        int Cadastrar(CadastroEmpresa cadastro);
+    }
+}
--- Salao.Domain/Service/Cliente/EmpresaService.cs
+++ Salao.Domain/Service/Cliente/EmpresaService.cs
@@ -1,15 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Salao.Domain.Abstract;
+﻿using Salao.Domain.Abstract;
 using Salao.Domain.Models.Cliente;
 using Salao.Domain.Repository;
+using System;
+using System.Linq;
 
 namespace Salao.Domain.Service.Cliente
 {
-    public class EmpresaService
+    public class EmpresaService: IBaseService<Empresa>
     {
+        private IBaseRepository<Empresa> repository;
+
+        public EmpresaService()
+        {
+            repository = new EFRepository<Empresa>();
+        }
+
+        public IQueryable<Empresa> Listar()
+        {
+            return repository.Listar();
+        }
+
+        public int Gravar(Empresa item)
+        {
+            // formata
+            item.Fantasia = item.Fantasia.ToUpper().Trim();
+            item.Contato = item.Contato.ToUpper().Trim();
+            item.Observ = item.Observ.ToUpper().Trim();
+            item.AlteradoEm = DateTime.Now;
+
+            // valida
+            if (item.TipoPessoa == 1 && string.IsNullOrEmpty(item.Cnpj))
+            {
+                throw new ArgumentException("Informe o CNPJ");
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(item.Cpf))
+                {
+                    throw new ArgumentException("CPF inválido");
+                }
+            }
+            if (item.Desconto > 0 && item.DescontoCarencia == 0)
+            {
+                throw new ArgumentException("Informe o número de meses de carência para desconto");
+            }
+
+            // grava
+            if (item.Id == 0)
+            {
+                item.Ativo = true;
+                item.CadastradoEm = DateTime.Now;
+                item.PrecoAvaliacao = 0;
+                item.ClienteAvaliacao = 0;
+                item.Aprovado = false;
+                item.Exibir = false;
+                return repository.Incluir(item).Id;
+            }
+
+            return repository.Alterar(item).Id;
+        }
+
+        public Empresa Excluir(int id)
+        {
+            try
+            {
+                return repository.Excluir(id);
+            }
+            catch (Exception)
+            {
+                // BD nao permite exclusao por FK, inativo
+                var empresa = repository.Find(id);
+                if (empresa != null)
+                {
+                    empresa.AlteradoEm = DateTime.Now;
+                    empresa.Ativo = false;
+                    return repository.Alterar(empresa);
+                }
+                return empresa;
+            }
+        }
+
+        public Empresa Find(int id)
+        {
+            return repository.Find(id);
+        }
     }
 }
