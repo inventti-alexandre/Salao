fa0752c133c3eaabd6b99964e0de4bc5fc598c4e
jbalessandro <jb.alessandro@gmail.com>
2015-09-24 18:22:05 -0300

    tipo endereco

--- Salao.Web/Areas/Admin/Controllers/TipoEnderecoController.cs
+++ Salao.Web/Areas/Admin/Controllers/TipoEnderecoController.cs
@@ -1,7 +1,9 @@
 ﻿using Salao.Domain.Abstract;
 using Salao.Domain.Abstract.Admin;
-using Salao.Domain.Models.Admin;
+using Salao.Domain.Abstract.Endereco;
+using Salao.Domain.Models.Endereco;
 using Salao.Domain.Service.Admin;
+using Salao.Domain.Service.Endereco;
 using System;
 using System.Linq;
 using System.Net;
@@ -10,76 +12,72 @@ using System.Web.Mvc;
 namespace Salao.Web.Areas.Admin.Controllers
 {
     [Authorize]
-    public class AreaController : Controller
+    public class TipoEnderecoController : Controller
     {
-        private IBaseService<Area> service;
+        IBaseService<EnderecoTipoEndereco> service;
         private ILogin login;
 
-        public AreaController()
+        public TipoEnderecoController()
         {
-            service = new AreaService();
+            service = new TipoEnderecoService();
             login = new UsuarioService();
         }
 
         //
-        // GET: /Admin/Area/
+        // GET: /Admin/TipoEndereco/
         public ActionResult Index()
         {
-            var areas = service.Listar()
+            var tipos = service.Listar()
                 .OrderBy(x => x.Descricao);
 
-            return View(areas);
+            return View(tipos);
         }
 
         //
-        // GET: /Admin/Area/Details/5
+        // GET: /Admin/TipoEndereco/Details/5
         public ActionResult Details(int id)
         {
-            var area = service.Find(id);
+            var tipo = service.Find(id);
 
-            if (area == null)
+            if (tipo == null)
             {
                 return HttpNotFound();
             }
 
-            return View(area);
+            return View(tipo);
         }
 
         //
-        // GET: /Admin/Area/Create
+        // GET: /Admin/TipoEndereco/Create
         public ActionResult Create()
         {
-            return View(new Area());
+            return View(new EnderecoTipoEndereco());
         }
 
         //
-        // POST: /Admin/Area/Create
+        // POST: /Admin/TipoEndereco/Create
         [HttpPost]
-        public ActionResult Create([Bind(Include="Descricao")] Area area)
+        public ActionResult Create([Bind(Include="Descricao")] EnderecoTipoEndereco tipo)
         {
             try
             {
-                area.AlteradoEm = DateTime.Now;
-                area.AlteradoPor = login.GetIdUsuario(System.Web.HttpContext.Current.User.Identity.Name);
-                TryUpdateModel(area);
-
                 if (ModelState.IsValid)
                 {
-                    service.Gravar(area);
+                    service.Gravar(tipo);
                     return RedirectToAction("Index");
                 }
 
-                return View(area);
+                return View(tipo);
             }
             catch (ArgumentException e)
             {
                 ModelState.AddModelError(string.Empty, e.Message);
-                return View(area);
+                return View();
             }
         }
 
         //
-        // GET: /Admin/Area/Edit/5
+        // GET: /Admin/TipoEndereco/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -87,44 +85,39 @@ namespace Salao.Web.Areas.Admin.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var area = service.Find((int)id);
+            var tipo = service.Find((int)id);
 
-            if (area == null)
+            if (tipo == null)
             {
                 return HttpNotFound();
             }
 
-            return View(area);
+            return View(tipo);
         }
 
         //
-        // POST: /Admin/Area/Edit/5
+        // POST: /Admin/TipoEndereco/Edit/5
         [HttpPost]
-        public ActionResult Edit([Bind(Include="Id,Descricao,Ativo")] Area area)
+        public ActionResult Edit([Bind(Include="Id,Descricao,Ativo")] EnderecoTipoEndereco tipo)
         {
             try
             {
-                area.AlteradoPor = login.GetIdUsuario(System.Web.HttpContext.Current.User.Identity.Name);
-                area.AlteradoEm = DateTime.Now;
-                TryUpdateModel(area);
-
                 if (ModelState.IsValid)
                 {
-                    service.Gravar(area);
-                    return RedirectToAction("Index");
+                    service.Gravar(tipo);
+                    return RedirectToAction("Index");                    
                 }
-
-                return View(area);
+                return View(tipo);
             }
             catch (ArgumentException e)
             {
                 ModelState.AddModelError(string.Empty, e.Message);
-                return View(area);
+                return View(tipo);
             }
         }
 
         //
-        // GET: /Admin/Area/Delete/5
+        // GET: /Admin/TipoEndereco/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -132,18 +125,18 @@ namespace Salao.Web.Areas.Admin.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var area = service.Find((int)id);
+            var tipo = service.Find((int)id);
 
-            if (area == null)
+            if (tipo == null)
             {
                 return HttpNotFound();
             }
 
-            return View(area);
+            return View(tipo);
         }
 
         //
-        // POST: /Admin/Area/Delete/5
+        // POST: /Admin/TipoEndereco/Delete/5
         [HttpPost]
         public ActionResult Delete(int id)
         {
@@ -155,12 +148,12 @@ namespace Salao.Web.Areas.Admin.Controllers
             catch (ArgumentException e)
             {
                 ModelState.AddModelError(string.Empty, e.Message);
-                var area = service.Find(id);
-                if (area == null)
+                var tipo = service.Find(id);
+                if (tipo == null)
                 {
                     return HttpNotFound();
                 }
-                return View(area);
+                return View(tipo);
             }
         }
     }
