5983b0aa1e3595a3cfb59a34a785a8abe6eb4888
jbalessandro <jb.alessandro@gmail.com>
2015-09-24 15:52:28 -0300

    repository

---
+++ Salao.Domain/Models/Cliente/CadastroEmpresa.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace Salao.Domain.Models.Cliente
+{
+    public class CadastroEmpresa
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome fantasia da empresa")]
+        [StringLength(60, ErrorMessage = "Máximo de 60 caracteres")]
+        [Display(Name = "Nome fantasia")]
+        public string Fantasia { get; set; }
+
+        [StringLength(60, ErrorMessage = "Máximo de 100 caracteres")]
+        [Display(Name = "Razão social")]
+        public string RazaoSocial { get; set; }
+
+        [Required]
+        [Range(1, 2, ErrorMessage = "Selecione o tipo de pessoa (física/jurídica)")]
+        [Display(Name = "Tipo de pessoa")]
+        [HiddenInput(DisplayValue = false)]
+        public Int16 TipoPessoa { get; set; }
+
+        [Display(Name = "CNPJ")]
+        public string Cnpj { get; set; }
+
+        [Display(Name = "CPF")]
+        public string Cpf { get; set; }
+
+        [Required]
+        [Range(1,2,ErrorMessage="Tipo de endereço inválido")]
+        [Display(Name="Tipo de endereço")]
+        public int TipoEndereco { get; set; }
+
+        [Required]
+        [Display(Name = "CEP")]
+        [StringLength(8, ErrorMessage = "O CEP é composto por 8 caracteres", MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [Required]
+        [Display(Name = "Logradouro")]
+        [StringLength(100, ErrorMessage = "O nome do logradouro é composto por no máximo 100 caracteres")]
+        public string Logradouro { get; set; }
+
+        [Required]
+        [Display(Name = "Número")]
+        [StringLength(10, ErrorMessage = "O número é composto por no máximo 10 caracteres")]
+        public string Numero { get; set; }
+
+        [StringLength(40, ErrorMessage = "O complemento é composto por no máximo 40 caracteres")]
+        public string Complemento { get; set; }
+
+        [Required]
+        [Display(Name="Bairro")]
+        [StringLength(100, ErrorMessage="O bairro é composto por no máximo 100 caracteres")]
+        public string Bairro { get; set; }
+
+        [Required]
+        [Display(Name = "Cidade")]
+        [StringLength(100, ErrorMessage = "A cidade é composto por no máximo 100 caracteres")]
+        public string Cidade { get; set; }
+
+        [Required]
+        [Display(Name = "Estado")]
+        [Range(1,99999,ErrorMessage="Selecione o Estado")]
+        public int IdEstado { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome para contato na empresa")]
+        [StringLength(60, ErrorMessage = "O nome do contato é composto por no máximo 60 caracteres")]
+        public string Contato { get; set; }
+
+        [Required(ErrorMessage = "Informe o e-mail")]
+        [StringLength(100, ErrorMessage = "Máximo de 100 caracteres")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe o DDD")]
+        [StringLength(20, ErrorMessage = "Máximo de 3 caracteres")]
+        public string DDD { get; set; }
+
+        [Required(ErrorMessage = "Informe o telefone")]
+        [StringLength(20, ErrorMessage = "Máximo de 20 caracteres")]
+        public string Telefone { get; set; }
+
+        [StringLength(6, ErrorMessage="Máximo de 6 caracteres")]
+        public string Ramal { get; set; }
+
+        [Display(Name = "Observações")]
+        [DataType(DataType.MultilineText)]
+        public string Observ { get; set; }
+
+        public bool Cortesia { get; set; }
+        public decimal Desconto { get; set; }
+
+        [Display(Name = "Carência (meses)")]
+        public int DescontoCarencia { get; set; }
+
+        public int CadastradoPor { get; set; }
+
+        [NotMapped]
+        public virtual string CadastradoPorNome
+        {
+            get
+            {
+                return new Domain.Service.Admin.UsuarioService().Find(CadastradoPor).Nome;
+            }
+        }
+
+        [NotMapped]
+        public virtual string Estado {
+            get
+            {
+                return new Domain.Service.Endereco.EstadoService().Find(IdEstado).UF;
+            }
+        }
+
+        [NotMapped]
+        public virtual string TipoEnderecoDescricao
+        {
+            get
+            {
+                return new Domain.Service.Endereco.TipoEnderecoService().Find(TipoEndereco).Descricao;
+            }
+        }
+
+    }
+}
---
+++ Salao.Domain/Service/Cliente/CadastroEmpresaService.cs
@@ -0,0 +1,167 @@
+﻿using Salao.Domain.Abstract;
+using Salao.Domain.Abstract.Admin;
+using Salao.Domain.Abstract.Endereco;
+using Salao.Domain.Models.Cliente;
+using Salao.Domain.Models.Endereco;
+using System;
+using System.Linq;
+
+namespace Salao.Domain.Service.Cliente
+{
+    public class CadastroEmpresaService : ICadastroEmpresa
+    {
+        IBaseService<Salao.Domain.Models.Endereco.Endereco> serviceEndereco;
+        IBaseService<Salao.Domain.Models.Cliente.Empresa> serviceEmpresa;
+        IBaseService<Salao.Domain.Models.Endereco.EnderecoEstado> serviceEstado;
+        IBaseService<Salao.Domain.Models.Endereco.EnderecoTelefone> serviceTelefone;
+        IBaseService<Salao.Domain.Models.Endereco.EnderecoEmail> serviceEmail;
+        IEnderecoService<Salao.Domain.Models.Endereco.EnderecoBairro> serviceBairro;
+        IEnderecoService<Salao.Domain.Models.Endereco.EnderecoCidade> serviceCidade;
+
+        public CadastroEmpresaService()
+        {
+            serviceEndereco = new Endereco.EnderecoService();
+            serviceEmpresa = new EmpresaService();
+            serviceEstado = new Salao.Domain.Service.Endereco.EstadoService();
+            serviceCidade = new Salao.Domain.Service.Endereco.CidadeService();
+            serviceBairro = new Salao.Domain.Service.Endereco.BairroService();
+            serviceTelefone = new Salao.Domain.Service.Endereco.TelefoneService();
+            serviceEmail = new Salao.Domain.Service.Endereco.EmailService();
+        }
+
+        public int ChecarCadastroAnterior(string documento)
+        {
+            return serviceEmpresa.Listar().Where(x => x.Cpf == documento || x.Cnpj == documento).Count();
+        }
+
+        public int Incluir(CadastroEmpresa cadastro)
+        {
+            int idEndereco = 0;
+            int idEmail = 0;
+            int idTelefone = 0;
+
+            try
+            {
+                // grava endereco
+                var endereco = new Salao.Domain.Models.Endereco.Endereco();
+                endereco.IdEstado = cadastro.IdEstado;
+                endereco.IdCidade = serviceCidade.GetId(cadastro.Cidade, cadastro.IdEstado);
+                endereco.IdBairro = serviceBairro.GetId(cadastro.Bairro, endereco.IdCidade);
+                endereco.Cep = cadastro.Cep;
+                endereco.Complemento = cadastro.Complemento;
+                endereco.IdTipoEndereco = cadastro.TipoEndereco;
+                endereco.Logradouro = cadastro.Logradouro;
+                endereco.Numero = cadastro.Numero;
+                endereco.Id = serviceEndereco.Gravar(endereco);
+                idEndereco = endereco.Id;
+
+                // grava email
+                if (!string.IsNullOrEmpty(cadastro.Email))
+                {
+                    var email = new EnderecoEmail { Email = cadastro.Email, IdEndereco = endereco.Id };
+                    idEmail = serviceEmail.Gravar(email);
+                }
+
+                // grava telefone
+                if (!string.IsNullOrEmpty(cadastro.Telefone))
+                {
+                    var telefone = new EnderecoTelefone { Contato = cadastro.Contato, DDD = cadastro.DDD, Ramal = cadastro.Ramal, Telefone = cadastro.Telefone, IdEndereco = idEndereco };
+                    idTelefone = serviceTelefone.Gravar(telefone);
+                }
+
+                // grava empresa
+                var empresa = new Empresa
+                {
+                    AlteradoEm = DateTime.Now,
+                    AlteradoPor = 0,
+                    CadastradoEm = DateTime.Now,
+                    CadastradoPor = cadastro.CadastradoPor,
+                    ClienteAvaliacao = 0,
+                    Cnpj = "" + cadastro.Cnpj,
+                    Contato = "" + cadastro.Contato,
+                    Cpf = "" + cadastro.Cpf,
+                    Cortesia = cadastro.Cortesia,
+                    Desconto = cadastro.Desconto,
+                    DescontoCarencia = cadastro.DescontoCarencia,
+                    IdEndereco = endereco.Id,
+                    Observ = "" + cadastro.Observ,
+                    Fantasia = cadastro.Fantasia,
+                    PrecoAvaliacao = 0,
+                    RazaoSocial = "" + cadastro.RazaoSocial,
+                    TipoPessoa = cadastro.TipoPessoa
+                };
+                return serviceEmpresa.Gravar(empresa);
+            }
+            catch (Exception ex)
+            {
+                serviceEmail.Excluir(idEmail);
+                serviceTelefone.Excluir(idTelefone);
+                serviceEndereco.Excluir(idEndereco);
+                throw new ArgumentException(ex.Message);
+            }
+
+            throw new ArgumentException("Não foi possível cadastrar esta empresa");
+        }
+
+        public int Alterar(CadastroEmpresa cadastro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empresa Excluir(int id)
+        {
+            return serviceEmpresa.Excluir(id);
+        }
+
+        public CadastroEmpresa Find(int id)
+        {
+            // apenas empresas que ainda possuem cadastro sujeito a aprovacao
+            var empresa = serviceEmpresa.Find(id);
+
+            if (empresa != null)
+            {
+                if (empresa.Aprovado == true || (empresa.Aprovado == false && empresa.AlteradoPor != 0))
+                {
+                    // esta empresa ja foi avalida
+                    throw new ArgumentException("Esta empresa já foi avalida anteriormente. Acesse o cadastro de empresas.");
+
+                }
+
+                var endereco = empresa.Endereco;
+                var telefone = endereco.Telefone;
+                var email = endereco.Email;
+
+                return new CadastroEmpresa
+                {
+                    Bairro = endereco.Bairro.Descricao,
+                    CadastradoPor = empresa.CadastradoPor,
+                    Cep = endereco.Cep,
+                    Cidade = endereco.Cidade.Descricao,
+                    Cnpj = empresa.Cnpj,
+                    Complemento = endereco.Complemento,
+                    Contato = empresa.Contato,
+                    Cortesia = empresa.Cortesia,
+                    Cpf = empresa.Cpf,
+                    DDD = telefone.DDD,
+                    Desconto = empresa.Desconto,
+                    DescontoCarencia = empresa.DescontoCarencia,
+                    Email = email.Email,
+                    Fantasia = empresa.Fantasia,
+                    Id = empresa.Id,
+                    IdEstado = endereco.IdEstado,
+                    Logradouro = endereco.Logradouro,
+                    Numero = endereco.Numero,
+                    Observ = empresa.Observ,
+                    RazaoSocial = empresa.RazaoSocial,
+                    Telefone = endereco.Telefone.Telefone,
+                    TipoEndereco = endereco.TipoEndereco.Id,
+                    TipoPessoa = empresa.TipoPessoa
+                };
+            }
+
+            return null;
+        }
+
+
+    }
+}
