92dc537e317cc6852be3b548ab04eb35c66160bb
jbalessandro <jb.alessandro@gmail.com>
2015-10-22 17:49:11 -0200

    servico

--- Salao.Web/Areas/Empresa/Controllers/ServicoController.cs
+++ Salao.Web/Areas/Empresa/Controllers/ServicoController.cs
@@ -5,7 +5,6 @@ using Salao.Domain.Service.Admin;
 using Salao.Domain.Service.Cliente;
 using Salao.Web.Areas.Empresa.Common;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
